7d887c605bd927b23bc8cea436efae7acfeb1719
Blizoman <xblizna00@stud.fit.vut.cz>
2026-04-08 19:05:34 +0200

    Added more tests and fixed Dockerfile

--- ipp26-project-templates/docker-entrypoint.sh
+++ ipp26-project-templates/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ln -sf $(which ruff) /src/int/ruff
-ln -sf $(which mypy) /src/int/mypy
-ln -sf $(which eslint) /src/tester/eslint
-ln -sf $(which prettier) /src/tester/prettier
+chmod +x /src/int/ruff
+chmod +x /src/int/mypy
+chmod +x /src/tester/eslint
+chmod +x /src/tester/prettier
 
 exec "$@"
